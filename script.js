aefc0b428ed61b28163aa789e7711568b3d07d44
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-05-14 09:11:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let personagem = getElementby.id(personagem);
+let classes = getElementby.id(classes);
+let arma = getElementby.id(arma);
+
+function lutar(){ 
+if(arma == ""){
+    console.log("tttt")
+}
+}
