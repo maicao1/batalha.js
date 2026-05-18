73275e732d092812fe36f528bb0afd16d5a5a9a1
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-05-18 09:09:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let personagem = getElementby.id(personagem);
-let classes = getElementby.id(classes);
-let arma = getElementby.id(arma);
 
 function lutar(){ 
-if(arma == ""){
-    console.log("tttt")
+
+
+const classes = getElementby.id('classes').value;
+const arma = getElementby.id('arma').value.trim();
+const log = getElementby.id('log');
+
+if(!arma){
+    log.innerhtml="Para de graça filha da puta"
+    return;
+//voce esqueceu de equipar uma arma
+    const dado = Math.floor(Math.random()*20)+1;
+    let resultado = `Dado: ${dado} | <strong>${classes}</strong> com <strong>${arma}</strong>`
 }
 }
